ac8e7c2a7c313a96da524ebf7a382466242f43e6
kobe <k.ankamah93@gmail.com>
2022-12-08 17:02:22 +0000

    endpoint change to type string

--- NetCoin/NetworkLayer/Endpoint.swift
+++ NetCoin/NetworkLayer/Endpoint.swift
@@ -1,5 +1,5 @@
 
 import Foundation
 struct Endpoint {
-   static let coinUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h"
+    static let coinUrl =  URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=24h")
 }
